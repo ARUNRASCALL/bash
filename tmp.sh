0af818d951cd6d2a07869b99c8ee4fd7f51da2a3
alba <alba@xnz.com>
2023-05-11 10:10:18 +0000

    trivial 2

--- tmp.sh
+++ tmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ####### shell script for installing tomcat#####
-sudo cd /opt
+sudo cd /opt/
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.8/bin/apache-tomcat-10.1.8.tar.gz
 sudo tar -zxvf apache-tomcat-10.1.8.tar.gz
 ####### end of shell script
